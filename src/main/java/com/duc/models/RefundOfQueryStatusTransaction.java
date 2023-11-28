140410959a53e76e69f097e8a16e1b5b558cfb8c
chientt <chientt@kaopiz.com>
2023-11-28 16:08:18 +0700

    remove unnecessary code

--- src/main/java/com/duc/models/RefundOfQueryStatusTransaction.java
+++
@@ -1,9 +0,0 @@
-package com.duc.models;
-
-public class RefundOfQueryStatusTransaction {
-    private String orderId;
-    private Long amount;
-    private Integer resultCode;
-    private Long transId;
-    private Long createdTime;
-}
